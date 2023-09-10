1a8a6f9e2fdd9f02e7489072ef7d05615250e142
Michal Litwa <Michal.Litwa@qiagen.com>
2023-09-10 16:33:30 +0200

    webpack update

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,24 +1,25 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
-module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime'],
-            },
-          },
-        },
-      ],
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: './public/index.html',
-        }),        
-      ]
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html',
     },
-  };
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies
+    })
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
